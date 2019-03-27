60cd79c363afd7b7abcf0b78e173a7472b812c8b
Ian Rose <ian.r.rose@gmail.com>
2019-03-27 13:53:28 -0700

    Add setup.

--- intake_dcat/__init__.py
+++ intake_dcat/__init__.py
@@ -1,1 +1,3 @@
 from .catalog import DCATCatalog
+
+_version = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,28 @@
+"""The setup script."""
+from setuptools import setup, find_packages
+from os.path import exists
+
+from intake_dcat._version import __version__ as version
+
+readme = open("README.md").read() if exists("README.md") else ""
+
+
+setup(
+    name='intake-dcat',
+    description='DCAT to Intake Catalog translation layer',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    maintainer='Ian Rose',
+    maintainer_email='ian.rose@lacity.org',
+    url='https://github.com/CityOfLosAngeles/intake-dcat',
+    packages=find_packages(),
+    package_dir={'intake-dcat': 'intake-dcat'},
+    include_package_data=True,
+    install_requires=[
+        'intake >= 0.4.3',
+    ],
+    license='Apache-2.0 license',
+    zip_safe=False,
+    keywords='intake dcat',
+    version=version,
+)
