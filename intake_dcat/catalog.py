3d6783266ec6d519a2dd01e824c7c6eec8078198
Ian Rose <ian.r.rose@gmail.com>
2019-03-27 15:50:32 -0700

    Add shapefile support, clean up open args generation.

--- intake_dcat/catalog.py
+++ intake_dcat/catalog.py
@@ -5,6 +5,7 @@ import requests
 from intake.catalog import Catalog
 from intake.catalog.local import LocalCatalogEntry
 
+from .distributions import get_relevant_distribution
 
 class DCATCatalog(Catalog):
     name: str
@@ -19,62 +20,17 @@ class DCATCatalog(Catalog):
         resp = requests.get(self.url)
         catalog = resp.json()
         self._entries = {
-            entry["identifier"]: _construct_entry(entry)
+            entry["identifier"]: construct_entry(entry)
             for entry in catalog["dataset"]
-            if _should_include_entry(entry)
+            if should_include_entry(entry)
         }
 
-def _get_relevant_distribution(dcat_entry):
-    """
-    Given a DCAT entry, find the most relevant distribution
-    for the intake catalog. In general, we choose the more specific
-    formats over the less specific formats. At present, they
-    are ranked in the following order:
-
-        GeoJSON
-        CSV
-
-    If none of these are found, None.
-    If there are no distributions, it returns None.
-    """
-    mediaTypes = ["application/vnd.geo+json", "text/csv"]
-    distributions = dcat_entry.get("distribution")
-
-    if not distributions or not len(distributions):
-        return None
-    for key in mediaTypes:
-        for distribution in distributions:
-            if distribution["mediaType"] == key:
-                return distribution
-    return None
+def should_include_entry(dcat_entry):
+    return get_relevant_distribution(dcat_entry) != None
 
 
-def _should_include_entry(dcat_entry):
-    return _get_relevant_distribution(dcat_entry) != None
-
-
-def _construct_entry(dcat_entry):
-    driver_map = {
-        "application/vnd.geo+json": "geojson",
-        "text/csv": "csv",
-    }
-    kwargs_map = {
-        "text/csv": {
-            "csv_kwargs": {
-                "blocksize": None,
-                "sample": False
-            }
-        }
-    }
-
+def construct_entry(dcat_entry):
+    driver, args = get_relevant_distribution(dcat_entry)
     name = dcat_entry["identifier"]
-    distribution = _get_relevant_distribution(dcat_entry)
-    mediaType = distribution["mediaType"]
-    url = distribution["downloadURL"]
-    args = {
-        "urlpath": url,
-        **(kwargs_map.get(mediaType) or {})
-    }
     description = f"## {dcat_entry['title']}\n\n{dcat_entry['description']}"
-    driver = driver_map[mediaType]
     return LocalCatalogEntry(name, description, driver, True, args=args, metadata=dcat_entry)
---
+++ intake_dcat/distributions.py
@@ -0,0 +1,90 @@
+import re
+
+def get_relevant_distribution(dcat_entry):
+    """
+    Given a DCAT entry, find the most relevant distribution
+    for the intake catalog. Returns a tuple of
+    (intake_driver_name, distribution). In general,
+    we choose the more specific formats over the less specific
+    formats. At present, they are ranked in the following order:
+
+        GeoJSON
+        Shapefile
+        CSV
+
+    If none of these are found, None.
+    If there are no distributions, it returns None.
+    """
+    distributions = dcat_entry.get("distribution")
+    if not distributions or not len(distributions):
+        return None
+
+    for d in distributions:
+        if test_geojson(d):
+            return 'geojson', geojson_driver_args(d)
+    for d in distributions:
+        if test_shapefile(d):
+            return 'shapefile', shapefile_driver_args(d)
+    for d in distributions:
+        if test_csv(d):
+            return 'csv', csv_driver_args(d)
+
+    return None
+
+
+def test_geojson(distribution):
+    """
+    Test if a DCAT:distribution is GeoJSON.
+    """
+    return distribution.get("mediaType") == "application/vnd.geo+json"
+
+
+def test_csv(distribution):
+    """
+    Test if a DCAT:distribution is CSV.
+    """
+    return distribution.get("mediaType") == "text/csv"
+
+
+def test_shapefile(distribution):
+    """
+    Test if a DCAT:distribution is a Shapefile.
+    """
+    # TODO: can there be a more robust test here?
+    url = distribution.get("downloadURL") or ""
+    title = distribution.get("title") or ""
+    return title.lower() == 'shapefile' or re.search('format=shapefile', url, re.I) != None
+
+
+def geojson_driver_args(distribution):
+    """
+    Construct driver args for a GeoJSON distribution.
+    """
+    url = distribution["downloadURL"]
+    return { "urlpath": url }
+
+
+def csv_driver_args(distribution):
+    """
+    Construct driver args for a GeoJSON distribution.
+    """
+    url = distribution["downloadURL"]
+    return {
+        "urlpath": url,
+        "csv_kwargs": {
+            "blocksize": None,
+            "sample": False,
+        }
+    }
+
+
+def shapefile_driver_args(distribution):
+    """
+    Construct driver args for a GeoJSON distribution.
+    """
+    url = distribution["downloadURL"]
+    args = { "urlpath": url, "geopandas_kwargs": {} }
+    if distribution["mediaType"] == "application/zip":
+        args["geopandas_kwargs"]["compression"] = "zip"
+    return args
+
